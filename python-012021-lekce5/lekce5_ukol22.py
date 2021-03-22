66122900f34a66b490ea0a8a5505344664537e92
MonaVana <monikavanzurova@seznam.cz>
2021-03-22 17:27:30 +0100

    ukoly 5. lekce

---
+++ python-012021-lekce5/lekce5_ukol22.py
@@ -0,0 +1,15 @@
+#import wget
+#wget.download("https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/5/character-deaths.csv")
+
+
+import pandas
+got = pandas.read_csv("character-deaths.csv")
+got = got.set_index("Name")
+
+
+#got.info()
+#got.index
+#print(got.loc["Hali"])
+#print(got.loc["Gevin Harlaw" : "Gillam"])
+#print(got.loc["Gevin Harlaw" : "Gillam", "Death Year"])
+print(got.loc["Gevin Harlaw" : "Gillam", "GoT" : "DwD"])
