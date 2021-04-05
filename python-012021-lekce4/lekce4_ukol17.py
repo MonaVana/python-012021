0f08492789ed51b087fd331223d7379216b4de2f
MonaVana <monikavanzurova@seznam.cz>
2021-04-05 18:56:28 +0200

    úkol 17 draft

---
+++ python-012021-lekce4/lekce4_ukol17.py
@@ -0,0 +1,45 @@
+class Polozka:
+  def __init__(self, nazev, zanr):
+    self.nazev = nazev
+    self.zanr = zanr
+
+class Film(Polozka):
+  def __init__(self, nazev, zanr, delka):
+    super().__init__(nazev, zanr)
+    self.delka = delka
+
+  def getCelkovaDelka(self):
+    celkovaDelka = self.delka
+    return celkovaDelka
+
+class Serial(Polozka):
+  def __init__(self, nazev, zanr, pocetEpizod, delkaEpizody):
+    super().__init__(nazev, zanr)
+    self.pocetEpizod = pocetEpizod
+    self.delkaEpizody = delkaEpizody
+
+  def getCelkovaDelka(self):
+    celkovaDelka = self.pocetEpizod * self.delkaEpizody
+    return celkovaDelka
+
+class Uzivatel:
+  def __init__(self, uzivatelskeJmeno):
+    self.uzivatelskeJmeno = uzivatelskeJmeno
+    self.delkaSledovani = 0
+
+  def pripoctiZhlednuti(self, celkovaDelka):
+    self.delkaSledovani += celkovaDelka
+
+  def getInfo(self):
+    return f"Uživatel {self.uzivatelskeJmeno} zhlédl celkem {self.delkaSledovani} minut."
+
+film = Film("Kmotr", "drama", 175)
+serial = Serial("Přátelé", "komedie", 236, 22)
+uzivatel = Uzivatel("Jan Novák")
+#print(film.getCelkovaDelka())
+#print(serial.getCelkovaDelka())
+uzivatel.pripoctiZhlednuti(film)
+print(uzivatel.getInfo())
+uzivatel.pripoctiZhlednuti(serial)
+print(uzivatel.getInfo())
+
